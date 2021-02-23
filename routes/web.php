25df8042d4c83b1ff62381c023d4e4f9612be2bb
Gowtham S <43443265+Gowtham135@users.noreply.github.com>
2021-02-23 19:15:20 +0530

    Formatted the code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::post('/message', [\App\Http\Controllers\ChatController::class, 'broadcast']);
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/message', [ChatController::class, 'broadcast'])->name('message');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
